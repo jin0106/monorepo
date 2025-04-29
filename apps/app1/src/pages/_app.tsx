0620bc773e4dee071cca57275be0a30144dd6f45
jin0106 <wjinh93@gmail.com>
2025-04-29 17:02:23 +0900

    ignore test

--- apps/app1/src/pages/_app.tsx
+++ apps/app1/src/pages/_app.tsx
@@ -68,7 +68,7 @@ const App = ({ Component, pageProps }: AppPropsWithLayout) => {
                 <ReactQueryDevtoolsProduction position="bottom-right" />
               </React.Suspense>
             )}
-            <div>staging1</div>
+            <div>app1</div>
             <Layout>
               <Component {...pageProps} />
             </Layout>
