2e3014802aa4a1d07c3e8891fa80b435d8bb85fd
jin0106 <wjinh93@gmail.com>
2025-04-29 17:05:28 +0900

    a

--- apps/app1/src/pages/_app.tsx
+++ apps/app1/src/pages/_app.tsx
@@ -68,7 +68,7 @@ const App = ({ Component, pageProps }: AppPropsWithLayout) => {
                 <ReactQueryDevtoolsProduction position="bottom-right" />
               </React.Suspense>
             )}
-            <div>app1</div>
+            <div>ignore test</div>
             <Layout>
               <Component {...pageProps} />
             </Layout>
