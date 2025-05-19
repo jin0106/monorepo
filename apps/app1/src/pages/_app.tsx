ecee860e626e5ca53c9611a14ed037b216d51399
jin0106 <wjinh93@gmail.com>
2025-05-19 14:57:52 +0900

    111

--- apps/app1/src/pages/_app.tsx
+++ apps/app1/src/pages/_app.tsx
@@ -68,7 +68,7 @@ const App = ({ Component, pageProps }: AppPropsWithLayout) => {
                 <ReactQueryDevtoolsProduction position="bottom-right" />
               </React.Suspense>
             )}
-            <div>deploty evsss111</div>
+            <div>deploty evsss11111</div>
             <Layout>
               <Component {...pageProps} />
             </Layout>
